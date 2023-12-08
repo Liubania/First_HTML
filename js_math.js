95206b25681b8ae9d6fde6d4546e50416c7d334c
Liubov Savinykh-Paltseva <147834732+Liubania@users.noreply.github.com>
2023-12-08 11:48:49 +0200

    Update js_math.js

--- js_math.js
+++ js_math.js
@@ -1,21 +1,40 @@
-let abc = document.getElementById("input1");
-abc.addEventListener("change", () => {
-  document
-    .getElementById("result")
-    .setAttribute("placeholder", document.getElementById("input1").value);
-});
+let inpmax=document.getElementById("input0");
+let inpmin=document.getElementById("input00");
+let first=document.getElementById("input1");
+let second=document.getElementById("input2");
 
-let resultInput = document.getElementById("result");
-resultInput.addEventListener("change", () => {
-  if (document.getElementById("result").value != null) {
+first.addEventListener ("change",()=>{
+document.getElementById("result").setAttribute("placeholder",first.value)
+
+})
+second.addEventListener ("change",()=>{
+document.getElementById("result").setAttribute("placeholder",second.value)
+
+ })
+
+let resultInput=document.getElementById("result");
+resultInput.addEventListener ("change",()=>{
+
+if (document.getElementById("result").value != null) {
     document.getElementById("check").disabled = false;
   }
-});
+})
+function getRandomInt(inpmin, inpmax) {
+min = Math.ceil(inpmin);
+max = Math.floor(inpmax);
+return Math.floor(Math.random() * (inpmax - inpmin + 1)) + inpmin;
+}
+
+let a=getRandomInt(inpmax,inpmin);
+let b=getRandomInt(inpmax,inpmin);
+
+first.value=a;
+second.value=b;
 
 function check() {
   let action = document.getElementById("action").value;
-  let a = document.getElementById("input1").value;
-  let b = document.getElementById("input2").value;
+  // let a = document.getElementById("input1").value;
+  // let b = document.getElementById("input2").value;
 
   document.getElementById("result").setAttribute("placeholder", a);
 
